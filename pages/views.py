c8bc9782d2be9418dd5eac09236c32bf9c27e66f
Mikle <michaels17525@mail.ru>
2020-09-22 12:25:48 +0300

    Add the new base template

--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 class homePageView(TemplateView):
     template_name = 'home.html'
+
+class aboutPageView(TemplateView):
+    template_name = 'about.html'
